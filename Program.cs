dccedd51fe24a4276281595d5fe599d0503769fb
jorgelcg <jorge.cardenastp@gmail.com>
2024-02-15 00:55:21 -0500

    addchanges

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+ void ConfigureServices(IServiceCollection services)
+{
+   
+    services.AddControllers();
+}
+builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +24,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action}/{id?}");
 
 app.Run();
 
