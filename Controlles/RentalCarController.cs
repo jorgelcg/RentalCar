53fd1be3cb6936757236c4ecd7532730a83381c9
jorgelcg <jorge.cardenastp@gmail.com>
2024-02-14 21:43:56 -0500

    controller

---
+++ Controlles/RentalCarController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentalCar.Controlles
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RentalCarController : ControllerBase
+    {
+        public RentalCarController()
+        { 
+        }
+        public IActionResult GetAllcars()
+        {
+
+            return Ok();
+        }
+    }
+}
